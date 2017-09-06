8ac4c8f40b6faab328e574e2443ae49b3ffd9411
nodgd <dcc1234@qq.com>
2017-09-06 20:54:57 +0800

    文件名规律性更强了

--- app/src/main/java/garbagemayor/bigenews/MainActivity.java
+++ app/src/main/java/garbagemayor/bigenews/MainActivity.java
@@ -2,12 +2,18 @@ package garbagemayor.bigenews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.MainActivityLayout);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.MainActivityMenu, menu);
+        return true;
     }
 }
